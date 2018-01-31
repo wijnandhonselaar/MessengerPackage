ae99f69c012d0be8c1fb2cf3169211b4026d84d1
Wijnand Honselaar <wijnandhonselaar@relatics.com>
2018-01-31 15:54:33 +0100

    Add project files.

---
+++ MessengerPackage/MessageTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace MessengerPackage
+{
+    public interface IMessage
+    {
+        string Name { get; set; }
+        string Message { get; set; }
+    }
+
+    /// <summary>
+    /// Use interface for sending message from the client to the server
+    /// </summary>
+    public interface IUnprocessedMessage : IMessage
+    {
+
+    }
+
+    /// <summary>
+    /// Use interface for sending message from the server to the clients
+    /// </summary>
+    public interface IProcessedMessage : IMessage
+    {
+
+    }
+}
